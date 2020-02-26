f0b28c4b02fa3a8ebf0583b2f3cdd45cccf1a5e5
sakshigholap <60745554+sakshigholap@users.noreply.github.com>
2020-02-26 10:15:16 -0500

    Update BasicStrategy.java

--- src/BasicStrategy.java
+++ src/BasicStrategy.java
@@ -1,11 +1,11 @@
-
 public class BasicStrategy extends Strategy {
 	public String getName() {
 		return "Basic Strategy";
 	}
 
+
 	@Override
-	public Rail returnRail(City[] targetCities, Rail[] possibleRails) {
+	public Rail returnRail(City[] targetCities, Rail[] possibleRails, Board wholeBoard) {
 		int index = (int)Math.random() * possibleRails.length;
 		return possibleRails[index];
 	}
