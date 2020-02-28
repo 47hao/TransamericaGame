7cd5acb14de5c984ecab3c9548dd3c1c4113bffe
slee9959 <60745546+slee9959@users.noreply.github.com>
2020-02-28 09:32:32 -0500

    deleted duplicate strategy

--- src/Strategy.java
+++ src/Strategy.java
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 
-public abstract class Strategy {
-
-	abstract int returnRail(City[] targetCities, Rail[] possibleRails[], Board[] wholeBoard); 
-	
-}
-=======
 public abstract class Strategy {
 
 	abstract int returnRail(City[] targetCities, Rail[] possibleRails, Board wholeBoard);
 	
 }
->>>>>>> branch 'master' of https://github.com/YourBoyConnor/GroupProject.git
+
