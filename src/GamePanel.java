54769ae75320b766c1d2b2b9b26a387e4ca17750
Connor Pymm <connor.pymm@gmail.com>
2020-03-03 09:29:31 -0500

    made a scoreboard track

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,4 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -13,44 +14,46 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements MouseListener {
-	
+
 	Game gameInfo;
 	int xMousePos = 0;
 	int yMousePos = 0;
 	Point mousePos = new Point(xMousePos, yMousePos);
-	
+
 	BufferedImage map;
-	
+
 	final int railLength = 20;
 	final int shortLength;
-	
+
 	final static int gridStartX = 360;
 	final static int gridStartY = 40;
-	
+
 	public GamePanel(Game game) {
 		gameInfo = game;
-		shortLength = (int)(railLength*0.1);
+		shortLength = (int) (railLength * 0.1);
 		try {
 			map = ImageIO.read(new File("img/map.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void paintComponent(Graphics g) {
 		drawBoard(g);
 		drawGrid(g);
 		drawScoreboard(g);
 		/*
-		for (Player p : gameInfo.getPlayers())
-			drawTrain(g, new Point(getPixelX(p.score), 50));
-		for (City c : gameInfo.getBoard().getCities())
-			drawCity(g, new Point(getPixelX(c.getPos().getX()), getPixelY(c.getPos().getY())));
-		for (Player p : gameInfo.getPlayers())
-			drawMarker(g, new Point(getPixelX(p.getMarkerPos().getX()), getPixelY(p.getMarkerPos().getY())));
-		for (Rail r : gameInfo.getBoard().getRails())
-			drawRails(g, new Point(getPixelX(r.startPos().getX()), getPixelY(r.startPos().getY())), new Point(getPixelX(r.endPos().getX()), getPixelY(r.endPos().getY())));
-		*/
+		 * for (Player p : gameInfo.getPlayers()) drawTrain(g, new
+		 * Point(getPixelX(p.score), 50)); for (City c :
+		 * gameInfo.getBoard().getCities()) drawCity(g, new
+		 * Point(getPixelX(c.getPos().getX()), getPixelY(c.getPos().getY()))); for
+		 * (Player p : gameInfo.getPlayers()) drawMarker(g, new
+		 * Point(getPixelX(p.getMarkerPos().getX()),
+		 * getPixelY(p.getMarkerPos().getY()))); for (Rail r :
+		 * gameInfo.getBoard().getRails()) drawRails(g, new
+		 * Point(getPixelX(r.startPos().getX()), getPixelY(r.startPos().getY())), new
+		 * Point(getPixelX(r.endPos().getX()), getPixelY(r.endPos().getY())));
+		 */
 		drawCityList(g, gameInfo.getBoard().getActivePlayer());
 		// draws board and game information
 	}
@@ -58,46 +61,70 @@ public class GamePanel extends JPanel implements MouseListener {
 	private void drawBoard(Graphics g) {
 		g.drawImage(map, 0, 0, this.getWidth(), this.getHeight(), Color.black, null);
 	}
-	
+
 	private void drawGrid(Graphics g) {
-		Graphics2D g2d = (Graphics2D)g;
-		for(Rail r : gameInfo.getBoard().getRails())
-		{
-			
+		Graphics2D g2d = (Graphics2D) g;
+		for (Rail r : gameInfo.getBoard().getRails()) {
+
 		}
 	}
-	
+
 	private void drawRail(Graphics g, Rail rail) {
 		g.setColor(Color.BLACK);
 		Point p = gridToPixel(rail.startPos());
 		Point p2 = gridToPixel(rail.endPos());
-		
-		if(rail.getState() == Rail.EMPTY)
-		{
-			if(rail.isDouble())
-			{
-				if(p.getY() == p2.getY())//horizontal rail
+
+		if (rail.getState() == Rail.EMPTY) {
+			if (rail.isDouble()) {
+				if (p.getY() == p2.getY())// horizontal rail
 				{
-					g.drawLine((int)p.getX(),(int)p.getY(),(int)(p.getX()+shortLength),(int)p.getY());
-					g.drawLine((int)p2.getX(),(int)p2.getY(),(int)(p2.getX()-shortLength),(int)p.getY());
-				} else if (p.getX() < p2.getX())//Southwest rail
+					g.drawLine((int) p.getX(), (int) p.getY(), (int) (p.getX() + shortLength), (int) p.getY());
+					g.drawLine((int) p2.getX(), (int) p2.getY(), (int) (p2.getX() - shortLength), (int) p.getY());
+				} else if (p.getX() < p2.getX())// Southwest rail
 				{
 				} else {
 				}
 			} else {
-				g.drawLine((int)p.getX(),(int)p.getY(),(int)p2.getX(),(int)p2.getY());
+				g.drawLine((int) p.getX(), (int) p.getY(), (int) p2.getX(), (int) p2.getY());
 			}
 		}
-			
+
 	}
 
-
 	private void drawScoreboard(Graphics g) {
-
+		//52.3076923077
+		Graphics2D g2 = (Graphics2D) g;
+		int xPos = 60;
+		double cellSize = 56.6666666667;
+		g.setColor(Color.WHITE);
+		g.fillRect(xPos, 20, 60, 30);
+		g.setColor(Color.BLACK);
+		g2.setStroke(new BasicStroke(7));
+		g.drawLine(60, 10, 60, 60);
+		g2.setStroke(new BasicStroke(5));
+		
+		for (int i = 1; i < 12; i++) {
+			xPos = 60 + (int)(i * cellSize);
+			g.setColor(Color.WHITE);
+			g.fillRect(xPos, 20, 59, 30);
+			g.setColor(Color.BLACK);
+			g.drawLine(xPos, 20, xPos, 50);
+			g.drawString(i + "", xPos - (int)(cellSize / 2) - (g.getFontMetrics().stringWidth(i + "") / 2), 35);
+		}
+		g2.setStroke(new BasicStroke(5));
+		g.drawLine(60, 20, this.getWidth() - 60, 20);
+		g.drawLine(60, 50, this.getWidth() - 60, 50);
+		
+		xPos = (int) Math.round(xPos + cellSize) - 1;
+		g.setColor(Color.WHITE);
+		g.fillRect(xPos, 15, 40, 40);
+		g.setColor(Color.BLACK);
+		g.drawRect(xPos, 15, 40, 40);
+		g.drawString(12 + "", xPos - (int)(cellSize / 2) - (g.getFontMetrics().stringWidth(12 + "") / 2), 35);
 	}
 
 	private void drawTrain(Graphics g, Point location) {
-		
+
 	}
 
 	private void drawCity(Graphics g, Point location) {
@@ -108,18 +135,16 @@ public class GamePanel extends JPanel implements MouseListener {
 
 	}
 
-	
 	private void drawCityList(Graphics g, Player p) {
 
 	}
-	
-	
+
 	private Point gridToPixel(Position pos) {
-		int yPos = (int)(pos.getY()*railLength*Math.cos(60)) + gridStartY;
-		int xPos = (pos.getX()*railLength) - (int)(0.5*pos.getY()*railLength) + gridStartX;
+		int yPos = (int) (pos.getY() * railLength * Math.cos(60)) + gridStartY;
+		int xPos = (pos.getX() * railLength) - (int) (0.5 * pos.getY() * railLength) + gridStartX;
 		return new Point();
 	}
-	
+
 	public void mouseMoved(MouseEvent e) {
 		xMousePos = e.getLocationOnScreen().x;
 		yMousePos = e.getLocationOnScreen().y;
@@ -150,5 +175,5 @@ public class GamePanel extends JPanel implements MouseListener {
 	public void mouseReleased(MouseEvent e) {
 
 	}
-	
+
 }
