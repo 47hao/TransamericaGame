7ef5c2d39ba5504e5959c6240f9d759faf5a6fda
nmarth2993 <nmarth2993@gmail.com>
2020-04-07 15:09:35 -0400

    Complete Endgame.java

--- src/EndGame.java
+++ src/EndGame.java
@@ -3,42 +3,103 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GridBagLayout;
+import java.awt.*;
 import javax.swing.*;
 
-public class EndGame extends JPanel {
-	JFrame frame;
-	JPanel panel;
-	JLabel title;
-	JLabel ranking;
-	private Scoreboard scoreboard;
+public class EndGame {
+	// final static int OFFSET_X = 20;
+	final static int OFFSET_Y = 30;
 
-	EndGame() {
+	JFrame frame;
+	EndPanel panel;
+
+	private Player[] players;
+
+	// private Scoreboard scoreboard;
+
+	// XXX: Scoreboard isn't needed because Game class will be able to provide
+	// XXX: the player array directly
+	public EndGame(Player[] p) {
+		players = p;
+		// scoreboard = new Scoreboard(p);
 		frame = new JFrame("Game Over");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		panel = new JPanel(new GridBagLayout());
+		panel = new EndPanel();
+		// panel.setBackground(new Color(233, 233, 233));
+		panel.setBackground(Color.WHITE);
 		frame.setContentPane(panel);
-		frame.setPreferredSize(new Dimension(600, 700));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel title = new JLabel("Game Over");
-		title.setFont(new Font("Arial", Font.BOLD, 36));
-		JLabel ranking = new JLabel("Final Ranking");
-		ranking.setForeground(Color.white);
-		EndGame a = new EndGame();
-		frame.add(a);
+		frame.setPreferredSize(new Dimension(600, 550));
+		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// JLabel title = new JLabel("Game Over");
+		// title.setFont(new Font("Arial", Font.BOLD, 36));
+		// JLabel ranking = new JLabel("Final Ranking");
+		// ranking.setForeground(Color.WHITE);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-
+		// panel.repaint();
 	}
 
-	public void paintComponent(Graphics g) {
-		// int temp=scoreboard.getWinnerOrder().size();
-		int yCoord = 350;
-		// for (int i=0; i<temp; i++) {
-		// g.drawString(scoreboard.getWinnerOrder().get(i), 300, yCoord+30);
+	class EndPanel extends JPanel {
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			System.out.println(getBackground());
+			Graphics2D g2d = (Graphics2D) g;
+
+			g2d.setColor(new Color(110, 70, 0));
+			for (int i = 0; i < 20; i++) {
+				if (i < 7 || i > 14) {
+					g2d.fillRect(3 + (getWidth() * i / 20), 0, 10, 45);
+				}
+			}
+			g2d.setColor(Color.BLACK);
+
+			g2d.setFont(new Font("Arial", Font.BOLD, 36));
+			g2d.drawString("Final Results:", panel.getWidth() / 3, 3 + OFFSET_Y);
+
+			Stroke stroke = g2d.getStroke();
+
+			g2d.setStroke(new BasicStroke(3f));
+			g2d.setColor(Color.GRAY);
+			g2d.drawLine(0, 3, getWidth(), 3);
+			g2d.drawLine(0, 40, getWidth(), 40);
+
+			g2d.setStroke(stroke);
+
+			g2d.setColor(Color.BLACK);
+			g2d.fillRect(0, OFFSET_Y + 30, panel.getWidth(), OFFSET_Y + 60 * 3);
+
+			for (int i = 0; i < players.length; i++) {
+				if (i == 0) {
+					g2d.setFont(new Font("Arial", Font.BOLD, 24));
+					g2d.setColor(new Color(233, 215, 0).brighter());
+				} else if (i == 1) {
+					g2d.setFont(new Font("Arial", Font.BOLD, 18));
+					g2d.setColor(Color.BLACK);
+					// fm = getFontMetrics(getFont());
+					// g2d.fillRect(panel.getWidth() / 5 - 5, OFFSET_Y + 70 - 5,
+					// fm.stringWidth(players[1].getName()),
+					// fm.getHeight());
+					g2d.setColor(new Color(192, 192, 192));
+				} else if (i == 2) {
+					g2d.setFont(new Font("Arial", Font.BOLD, 16));
+					g2d.setColor(new Color(205, 127, 50));
+				} else {
+					g2d.setFont(new Font("Arial", Font.BOLD, 14));
+					g2d.setColor(Color.BLACK);
+				}
+				g2d.drawString((i + 1) + ". " + players[i].getName(), panel.getWidth() / 5, OFFSET_Y + 70 * (1 + i));
+			}
+		}
 	}
 
-	// }
+	public static void main(String[] args) {
 
+		SwingUtilities.invokeLater(() -> {
+			new EndGame(new Player[] { new HumanPlayer("Human", Color.CYAN), new HumanPlayer("Human 2", Color.YELLOW),
+					new HumanPlayer("Human 3", Color.GREEN), new HumanPlayer("Loser", Color.GREEN),
+					new HumanPlayer("Loser", Color.GREEN), new HumanPlayer("Loser", Color.GREEN) });
+		});
+	}
 }
